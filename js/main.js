9f8304facc68848016a8c64a50f38a6ab3c9c29f
DanDan <1576168346@qq.com>
2016-07-10 18:22:09 +0800

    '第一次提交'

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function onclickbutton()
+{
+	alert('fdsf');
+	var buttons = input.parentElement.getElementsByTagName("input");
+	
+	for(int i=0;i<buttons.length;i++)
+	{
+		buttons[i].className={ background-color: white;}
+	}
+	event.srcElement.className={ background-color: rgb(226,28,1);}
+}
+
